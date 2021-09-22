646e76342ed9c654bd60bb705f81260d9cbdf19a
Andrew Waring <andy@fishpoolmarketing.com>
2021-09-22 12:01:24 +0100

    Create org-json.js

---
+++ org-json.js
@@ -0,0 +1,26 @@
+<script type='application/ld+json'>
+{
+  "@context": "http://www.schema.org",
+  "@type": "LocalBusiness",
+  "name": "White Bear Studios - Recording Studio Manchester",
+  "url": "https://www.whitebearstudios.co.uk/",
+  "telephone" : "+4474674352909",
+  "sameAs": [
+     "https://www.facebook.com/whitebearstudios/",
+     "https://www.instagram.com/whitebearstudios/",
+     "https://www.youtube.com/watch?v=ebN6aZUwLQ8",
+     "https://soundcloud.com/whitebearstudios"
+  ],
+  "logo": "https://static.wixstatic.com/media/affb31_b24037d9d26943188fe249e87416307a~mv2.png",
+  "image": "https://static.wixstatic.com/media/affb31_d582529a237d406fb9d46322011bcb04~mv2.jpg",
+  "description": "Recording, Music Production, Mixing & Mastering & Sound Design studio based in Manchester UK ran by producer and mix engineer David Page. I record and mix a wide range of clients including bands, solo artists, voice overs, choirs and orchestras. I also specialise in music composition and arrangement for clients looking to take their music to the next level. I also work as a sound designer for video including infomercials, social media content and advertising.",
+  "address": {
+     "@type": "PostalAddress",
+     "streetAddress": "Hope Mill, 113 Pollard Street",
+     "addressLocality": "Manchester",
+     "postalCode": "M4 7JB",
+     "addressCountry": "United Kingdom"
+  },
+   "openingHours": "Mo 14:00-18:00 Tu 10:00-18:00 We 10:00-18:00 Th 10:00-19:00 Fr 10:00-18:00"
+}
+</script>
